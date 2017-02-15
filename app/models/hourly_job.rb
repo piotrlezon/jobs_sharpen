4b86fbdbdb28d655712d333cfcf97579448d40dd
Piotr Lezon <piotr.lezon@u2i.com>
2017-02-15 11:01:00 +0100

    Make job fail

--- app/models/hourly_job.rb
+++ app/models/hourly_job.rb
@@ -1,7 +1,17 @@
 class HourlyJob < ApplicationRecord
-  enum status: [:initial, :completed]
+  enum status: [:initial, :completed, :failed]
 
   def run
+    run!
     completed!
+  rescue
+    failed!
+  rescue Exception
+    failed!
+    raise
   end
+
+  private
+
+  def run!; end
 end
