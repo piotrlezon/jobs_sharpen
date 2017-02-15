156c142602f56b3bc8f953c9f1a48f63d0fbc311
Piotr Lezon <piotr.lezon@u2i.com>
2017-02-15 16:45:08 +0100

    Make job abort after 3 failures

--- app/models/hourly_job.rb
+++ app/models/hourly_job.rb
@@ -1,14 +1,20 @@
 class HourlyJob < ApplicationRecord
-  enum status: [:initial, :completed, :failed]
+  MAX_ALLOWED_FAILURES = 3
+
+  enum status: [:initial, :completed, :failed, :aborted]
 
   def run
     run!
     completed!
   rescue
-    failed!
+    update!(failure_count: failure_count + 1, status: status_when_failed)
   end
 
   private
 
   def run!; end
+
+  def status_when_failed
+    failure_count >= MAX_ALLOWED_FAILURES ? :aborted : :failed
+  end
 end
