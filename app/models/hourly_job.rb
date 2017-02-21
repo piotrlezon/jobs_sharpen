fb838a9687a9cc1715d20de0bf97d3ba6f120332
Piotr Lezon <piotr.lezon@u2i.com>
2017-02-21 20:38:19 +0100

    Make creating new jobs

--- app/models/hourly_job.rb
+++ app/models/hourly_job.rb
@@ -6,6 +6,10 @@ class HourlyJob < ApplicationRecord
 
   scope :to_run, -> { where(status: statuses.values_at(:initial, :failed)).order(:time) }
 
+  def self.create_new_jobs
+    where(time: Time.zone.now.beginning_of_hour).first_or_create
+  end
+
   def self.run
     while (hourly_jobs = to_run).present?
       hourly_jobs.each do |hourly_job|
