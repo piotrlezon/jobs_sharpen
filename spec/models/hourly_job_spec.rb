95871b3fb68575e71a62914b99ba7e8b0e2f8bf2
Piotr Lezon <piotr.lezon@u2i.com>
2017-02-21 13:54:32 +0100

    Test job runs exclusively

--- spec/models/hourly_job_spec.rb
+++ spec/models/hourly_job_spec.rb
@@ -59,4 +59,46 @@ RSpec.describe HourlyJob do
       end
     end
   end
+
+  describe '#run_exclusively' do
+    subject(:run_hourly_job_exclusively) { hourly_job.run_exclusively }
+
+    context 'when job is initial' do
+      let(:hourly_job) { create(:initial_hourly_job) }
+
+      context 'when called multiple times' do
+        before { expect(ActiveRecord::Base.connection.pool.size).to be >= 5 }
+
+        it 'runs job only once', no_transactionial_db_cleaner: true do
+          expect(hourly_job).to receive(:run).once.and_call_original
+          keep_waiting = true
+          threads = (ActiveRecord::Base.connection.pool.size - 1).times.map do
+            Thread.new do
+              true while keep_waiting
+              hourly_job.run_exclusively # TODO subject can be called a single time only
+            end
+          end
+          keep_waiting = false
+          threads.each(&:join)
+        end
+      end
+
+      it 'runs the job' do
+        # TODO should this spec test that the run method got called or that the status changed to completed?
+        expect(hourly_job).to receive(:run).and_call_original
+        run_hourly_job_exclusively
+      end
+    end
+
+    described_class.statuses.except(:initial).each do |status_name, status_value|
+      context "when job is #{status_name}" do
+        let(:hourly_job) { create(:hourly_job, status: status_value) }
+
+        it 'does not run the job' do
+          expect(hourly_job).not_to receive(:run)
+          run_hourly_job_exclusively
+        end
+      end
+    end
+  end
 end
